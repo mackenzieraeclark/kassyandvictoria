e7e9c7b4624e588765f2ba0f1f3119b4de01577f
Mackenzie Clark <clark.rmackenzie@gmail.com>
2021-02-23 01:50:46 -0500

    footer image

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,12 @@
 import React from "react";
 
+import happyEverAfter from '../Assets/happyEverAfter.jpeg'
+
 export default function Footer({ id }) {
   return (
     <div className="container">
-      <div id={id}>
-        Footer
+      <div className="footer" id={id}>
+        <img src={happyEverAfter} alt="And they lived happily ever after." />
       </div>
     </div>
   );
