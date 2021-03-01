88c70bf73f9fb37550d1dd2409237e5bd346bc95
Mackenzie Clark <clark.rmackenzie@gmail.com>
2021-03-01 18:05:07 -0500

    larger wishing well section

--- src/Components/Registry.js
+++ src/Components/Registry.js
@@ -6,24 +6,17 @@ export default function Registry({ id }) {
       <div id={id}>
        <div className="row card">
          <div className="card-content">
-           <span style={{fontFamily: 'cursive', fontSize: 'x-large'}}>Wishing Well</span>
-          <br/><br/>
-           More than just kisses so far we've shared
-           <br/>
-           Our home has been made with love and care,
-           <br/>
-           Most things we need we've already got,
-           <br/>
-           And in our home we can't fit a lot!
-           <br/>
-           A donation to our wishing well would be great,
-           <br/>
-           But only if you wish to participate.
-           <br/><br/>
-           <button type="submit">
-             <a href='https://registry.theknot.com/kassandra-perez-victoria--sachs-november-2021-fl/43496472' >Donate</a>
-            </button>
-           <br/><br/>
+              <span style={{fontFamily: 'cursive', fontSize: 'x-large'}}>Wishing Well</span>
+              <h4>More than just kisses so far we've shared <br/>
+              Our home has been made with love and care,<br/>
+              Most things we need we've already got,<br/>
+              And in our home we can't fit a lot!<br/>
+              A donation to our wishing well would be great,<br/>
+              But only if you wish to participate.</h4>
+              <button type="submit">
+                <a href='https://registry.theknot.com/kassandra-perez-victoria--sachs-november-2021-fl/43496472' >Donate</a>
+                </button>
+              <br/><br/>
          </div>
        </div>
       </div>
