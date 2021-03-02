d7b51b2194c328bfe49fe8215141c5066cd7ac2f
Mackenzie Clark <clark.rmackenzie@gmail.com>
2021-03-01 20:30:18 -0500

    grammar fix in story

--- src/Components/Story.js
+++ src/Components/Story.js
@@ -11,7 +11,7 @@ export default function Story({ id }) {
         <div className="row">
          <div className="column">
           <br /><br />
-          <h4>For those who don’t know, Kassy and Victoria met back in 2015 at a Criminology Fraternity. Victoria noticed a girl trying to dribble a ball with her knees and was immediately consumed by the idea that that girl may play soccer. (Shocker, am I right?) That girl, Kassy, has two left feet and couldn’t play soccer if she tried. After that day, Kassy would go to events and would run into Victoria. The two slowly developed a friendship. Victoria was just <em>pretending</em> to not be interested but Kassy was being patient. Quickly, the two grew closer and closer. Victoria being the stubborn person that she is (again, shocker) kept playing hard to get. </h4>
+          <h4>For those who don’t know, Kassy and Victoria met back in 2015 in a Criminology Fraternity. Victoria noticed a girl trying to dribble a ball with her knees and was immediately consumed by the idea that that girl may play soccer. (Shocker, am I right?) That girl, Kassy, has two left feet and couldn’t play soccer if she tried. After that day, Kassy would go to events and would run into Victoria. The two slowly developed a friendship. Victoria was just <em>pretending</em> to not be interested but Kassy was being patient. Quickly, the two grew closer and closer. Victoria being the stubborn person that she is (again, shocker) kept playing hard to get. </h4>
           <br /><br />
           <img src={ourStory2} alt="we said yes!" style={{width: '60%'}} />
           <br /><br />
@@ -19,7 +19,7 @@ export default function Story({ id }) {
         </div>
         <div className="row">
           <div className="column">
-            <h4>Eventually, that all changed on Cinco de Mayo in 2016. Victoria FINALLY admitted that she had feelings for Kassy in a drunken ramble. A beach day later and some devising from a meddling best friend, the two finally expressed their feelings for each other.</h4>
+            <h4>Eventually, that all changed on Cinco de Mayo in 2016. Victoria <em>finally</em> admitted that she had feelings for Kassy in a drunken ramble. A beach day later and some devising from a meddling best friend, the two finally expressed their feelings for each other.</h4>
             <br /><br />
             <img src={ourStory1} alt="Two dogs later..." style={{width: '60%'}} />
             <br /><br />
