e2dd6d805f8fb679d63759986c1453ba7ddaa447
Mackenzie Clark <clark.rmackenzie@gmail.com>
2021-03-01 20:55:31 -0500

    logic adjusted for mobile and desktop cover Hero image

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,31 +1,59 @@
 import React from "react";
 
-import helloHero from '../Assets/hero.jpeg';
-var divStyle = {
-  color: 'white',
-  margin: "0",
-  width: "100vw",
-  height: "100vh",
-  backgroundImage: 'url(' + helloHero + ')',
-  backgroundSize: 'cover'
-};
+import desktopImage from '../Assets/desktopHero.jpeg';
+import mobileImage from '../Assets/mobileHero.jpeg';
 
-export default function Hero({ id }) {
+const Hero = () => {
+  const imageUrl = window.innerWidth >= 650 ? desktopImage : mobileImage;
+
   return (
-    <div className="container">
-      <div id={id}>
-      <div style={divStyle}>
-        <div style={{ 
-          textAlign: 'center', 
-          alignItems: 'center',
-          justifyContent: 'center',
-          paddingTop: '5em' }}>
+      <div className="Hero" style={{backgroundImage: `url(${imageUrl})` }}>
+          <div className="Hero-content">
           <h3>Join us this November 12th in celebrating...</h3>
           <br/><br/>
           <h1>Kassy and Victoria</h1>
-        </div>
+          </div>
       </div>
-      </div>
-    </div>
   );
-}
+};
+
+export default Hero;
+
+
+
+
+
+
+
+
+
+
+
+// var divStyle = {
+//   color: 'white',
+//   margin: "0",
+//   width: "100vw",
+//   height: "100vh",
+//   backgroundImage: 'url(' + helloHero + ')',
+//   backgroundSize: 'cover'
+// };
+
+// export default function Hero({ id }) {
+//   return (
+//     <div className="container">
+//       <div id={id}>
+//       <div style={divStyle}>
+//         <div style={{ 
+//           textAlign: 'center', 
+//           alignItems: 'center',
+//           justifyContent: 'center',
+//           paddingTop: '5em' }}>
+//           <h3>Join us this November 12th in celebrating...</h3>
+//           <br/><br/>
+//           <h1>Kassy and Victoria</h1>
+//         </div>
+//       </div>
+//       </div>
+//     </div>
+//   );
+// }
